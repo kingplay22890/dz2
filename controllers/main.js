004544433955a6cb5f12c69af317717b44243fba
kingplay22890 <narusss101@gmail.com>
2024-05-13 22:24:31 +0600

    Initial commit

---
+++ controllers/main.js
@@ -0,0 +1,14 @@
+const staticFile  = require("../appModules/http-utils/static-file");    
+const { getData, endpoints } = require("../appModules/api");
+// const endpoints = require("../appModules/api/config");
+const { makeRatingFile, config } = require("../rating")
+
+
+async function mainRouteController(res, publicUrl, extname) {
+  const data =  await getData(endpoints.games);
+
+  await makeRatingFile(config.PATH_TO_RATING_FILE , data);
+    res.statusCode = 200;
+    staticFile(res, publicUrl, extname);
+  }
+  module.exports = mainRouteController;
